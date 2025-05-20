413253d811cc27a84c64ab25ad965c2d91ad1e0f
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-05-20 17:17:16 +0530

    input

---
+++ 07-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ Certificate=("Az104" "AZ305" "MS102" "AZ400")
+ echo "First certificate $(Certificate{0})"
+ echo "Third certificate $(certificate{3})"
+
+ echo "print all certificate: $(certificate{@})"
+ 
