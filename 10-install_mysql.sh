e381bf6e575dd7209681e60ff4e57e43cf8277c4
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-05-24 12:08:43 +0530

    if condition

---
+++ 10-install_mysql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+userid=$(id -u)
+if [ $userid -ne 0 ]
+echo " ERROR: Run with root access"
+else
+echo " you are in root access"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+echo "mysql unable to install"
+exit 1
+else
+echo "mysql successfully installed"
+fi
