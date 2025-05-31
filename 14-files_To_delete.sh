a90bf759bf5326b76e566cbf956f421ac9c1ce30
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-05-31 12:40:30 +0530

    delete

--- 14-files_To_delete.sh
+++ 14-files_To_delete.sh
@@ -2,7 +2,7 @@
 source_dir=/home/ec2-user/app-logs
 files_to_delete=$(find $source_dir . -name "*.log" -mtime +14)
 
-while IFS= read -r $filepath
+while IFS= read -r  filepath
 do
   rm -rf $filepath
   done <<< $files_to_delete
