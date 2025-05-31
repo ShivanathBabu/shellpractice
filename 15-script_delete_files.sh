25c525d914fc883053117383a2786b5a0c7510e6
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-05-31 13:19:53 +0530

    delete

--- 15-script_delete_files.sh
+++ 15-script_delete_files.sh
@@ -6,15 +6,15 @@ y="\e[33m"
 n="\e[0m"
 
 logs_folder="/var/log/shell-logs"
-script_folder=$($0 | cut -d "." -f1)
-log_file= $logs_folder/$script_folder
+script_folder=$( echo $0 | cut -d "." -f1)
+log_file= "$logs_folder/$script_folder.log"
 source_dir=/home/ec2-user/app-logs
 
 mkdir -p $logs_folder
 
 if [ $userid -ne 0 ]
 then
-echo "$r: ERROR: run with sudo access $n"
+echo -e "$r ERROR: run with sudo access $n"
 else
 echo "you are in sudo access"
 fi
@@ -23,9 +23,9 @@ validate()
 {
     if [ $1 -eq 0 ]
     then
-    echo "$2 is $g succes $n"
+    echo -e "$2 is $g succes $n"
     else
-    echo "$2 is $r failure $n"
+    echo -e "$2 is $r failure $n"
 }
 echo "script started at $(date)"
 files=$(find $source_dir -name "*.log" -mtime 14)
