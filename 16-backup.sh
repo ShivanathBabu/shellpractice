8bcd1eb575c88303f4579a6d5eccc8b85397b72c
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-05-31 20:33:24 +0530

    backup

--- 16-backup.sh
+++ 16-backup.sh
@@ -7,8 +7,6 @@ logs_folder="/var/log/backup-logs"
 script_name=$( echo $0 | cut -d "." -f1 )
 log="$logs_folder/$script_name.log"
 
-
-
 r="\e[31m"
 g="\e[32m"
 y="\e[33m"
@@ -63,6 +61,29 @@ echo -e "$r source directory $desti_dir does not exit. please check $n"
 exit 1
 fi
 
-# files=$(find $source_dir -name "*.log" -mtime +$days)
+files=$(find $source_dir -name "*.log" -mtime +$days)
 
-# if[ !-]
+if[ !-z $files ]
+then
+echo "Files to zip: $files"
+timestamp=$(date + %f -%h -%m -%s)
+zip_file="$desti_dir/app-logs-$timestamp.zip"
+find $source_dir -name "*.log" -mtime +$days | zip -@ "$zip_file"
+
+if [ -f $zip_file ]
+then
+echo -e "$g successfully created zip file $n"
+while IFS=read -r filepath
+do
+echo "deleting files: $filepath" | tee -a $log
+rm -rf $filepath
+done <<< $files
+
+echo -e "log files older than $days from source directory removed...$g success $n"
+else
+echo -e "zip files creation....$r failure $n"
+exit 1
+fi
+else
+echo -e "no log files found older than 14 days...$y skipping $n"
+fi
