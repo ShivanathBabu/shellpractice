8a82a1263847b6e41ecbec8b99241109b183b6f7
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-07 19:08:59 +0530

    shell-practice

--- delete-file.sh
+++ delete-file.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
-source_dir=/home/ec2-user/app-logs
-file_to_delete=$(find $source_dir -name "*.log" -mtime +14)
-while IFS= read -r file
-do
-  echo "deleting files: $file"  
-  rm -rf $file
-  if [ $? -ne 0 ]
-  then
-  echo "failed to delete files"
-  else
-  echo  "successfully deleted files"
-  fi
+# #!/bin/bash
+# source_dir=/home/ec2-user/app-logs
+# file_to_delete=$(find $source_dir -name "*.log" -mtime +14)
+# while IFS= read -r file
+# do
+#   echo "deleting files: $file"  
+#   rm -rf $file
+#   if [ $? -ne 0 ]
+#   then
+#   echo "failed to delete files"
+#   else
+#   echo  "successfully deleted files"
+#   fi
  
- done <<< $file_to_delete
+#  done <<< $file_to_delete
+
+#!/bin/bash
+source_dir="/home/ec2-user/app-logs"
+
+find "$source_dir" -name "*.log" -mtime +14 | while IFS= read -r file
+do
+  echo "Deleting file: $file"
+  rm -f "$file"
+done
