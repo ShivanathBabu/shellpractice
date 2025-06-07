62513ec655efd936bae186ce54c22b40a0100676
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-07 18:52:39 +0530

    shell-practice

--- delete-file.sh
+++ delete-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source_dir=/home/ec-user/app-logs
-file_to_delete=$(find $source_dir -name "*.log" -mtime +14 days)
+file_to_delete=$(find $source_dir -name "*.log" -mtime +14)
 while IFS= read -r file
 do
  rm -rf $file
