cb8718606a02ebd1da94fd75c323839333b8682f
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-08 13:12:47 +0530

    disk usage

--- disk.sh
+++ disk.sh
@@ -3,13 +3,14 @@ disk_usage=$(df -hT | grep -v Filesystem)
 disk_thrishold=1
 MSG=" "
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
 while IFS= read line
 do
  usage=$(echo $line | awk '{print $6f}' | cut -d "%" -f1 )
  partition=$(echo $line | awk '{print $7f}')
  if [ $usage -ge $disk_thrishold ]
  then
-  msg+= "high disk usage on $partition: $usage %"
+   MSG+="High Disk Usage on $PARTITION: $USAGE % <br>"
   fi
 done <<< $disk_usage
 
