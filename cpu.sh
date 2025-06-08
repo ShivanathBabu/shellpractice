7afabd7afd74119c71e4600c0c89083c9cd86268
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-08 18:07:32 +0530

    cpu

--- cpu.sh
+++ cpu.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 while true; do
-cpu_idle=$(top -b -n2 | grep "Cpu(s)" | tail -1 | awk-F ',' '{print $4}'|awk '{print $1}')
+cpu_idle=$(top -b -n2 | grep "Cpu(s)" | tail -1 | awk -F ',' '{print $4}'|awk '{print $1}')
 cpu_utilization=$(echo "scale=1; 100 - $cpu_idle" | bc)
 echo "cpu usage": ${cpu_utilization}%"
 sleep 1
 done
-
