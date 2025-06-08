1f4f993c73010a36947cbacec6844118607e428a
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-08 18:27:31 +0530

    cpu

--- memory.sh
+++ memory.sh
@@ -2,8 +2,8 @@
 
 while true; do
 mem_data=$(free -m | grep Mem)
-total=$(echo $mem_data | awk -f '{print $2f}')
-used=$(echo $mem_data | awk -f '{print $3f}')
+total=$(echo $mem_data | awk '{print $2}')
+used=$(echo $mem_data | awk  '{print $3}')
 usage=$(echo "$scale=1; ($used/$total)*100" | bc)
 echo "memory usage: ${usage}%"
 sleep 1
