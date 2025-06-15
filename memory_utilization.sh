c77e1a2293a87f4cc047203809ee94861eb85134
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-15 21:43:33 +0530

    memory usage

---
+++ memory_utilization.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+memory_usage=$( free | awk '/Mem/{printf "%.0f" $3/$2*100}')
+Threshold=75
+echo "current memory usage: $memory_usage%"
+if [ "$memory_usage" -gt "$Threshold" ]
+then
+echo "Alert memory usage above ${Threshold}%!"
+else
+echo "memory usage is within limit"
+fi
