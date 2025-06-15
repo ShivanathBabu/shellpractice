05440a90f3d0b4f6148d7f8cc73013bc35590137
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-15 22:00:03 +0530

    cpu usage

--- cpu_test.sh
+++ cpu_test.sh
@@ -3,7 +3,7 @@ cpu_usage=$( top -bn1 | grep "Cpu(s)" | awk '{printf 100-$8}')
 cpu_int=${cpu_usage%.*}
 if [ "$cpu_int" -gt 75 ]
 then
-echo "$(date '%Y-%M-%d %H:%M:%S')-High Cpu usage ${cpu_usage}%"
+echo "$(date '%Y-%m-%d %H:%M:%S')-High Cpu usage ${cpu_usage}%"
 else
-echo "$(date '%Y-%M-%S %H:%M:S')- cpu usage is normal ${cpu_usage}%"
+echo "$(date '%Y-%m-%S %H:%M:S')- cpu usage is normal ${cpu_usage}%"
 fi 
