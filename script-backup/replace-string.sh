cbed0fa4a92912ac2e6f35259a4f95e0be79b862
ShivanathBabu <shivanathbabu.kodimyala@gmail.com>
2025-07-19 16:04:43 +0530

    updated

---
+++ script-backup/replace-string.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+old = "hello"
+new = "Update"
+
+# loop all .txt files
+for file in *.txt; do
+if [ -f "$file" ]; # check its regular file
+then
+echo "processing $file"
+sed -i "s/$[old]/$[new]/g" "$file"
+fi 
+done
