bfbe96af1ce1cac44b012d544952d03fcb494ab7
ShivanathBabu <shivanathbabu.kodimyala@gmail.com>
2025-07-19 16:53:54 +0530

    updated

--- script-backup/replace-string.sh
+++ script-backup/replace-string.sh
@@ -5,7 +5,7 @@ new="Update"
 target_dir="/home/ec2/files"
 # loop all .txt files
 for file in "$target_dir"/*.txt; do
-if [ -f "$file" ]; # check its regular file
+if [ -f "$file" ] # check its regular file
 then
 echo "processing $file"
 sed -i "s/$old/$new/g" "$file"
