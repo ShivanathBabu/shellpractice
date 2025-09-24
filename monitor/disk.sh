e7fb4a6cca53f4cc0489bad7f9cd092092aabeb4
ShivanathBabu <shivanathbabu.kodimyala@gmail.com>
2025-09-24 18:41:44 +0530

    shellscripts

--- monitor/disk.sh
+++ monitor/disk.sh
@@ -2,7 +2,7 @@
 disk_usage=$(df -hT | grep -v Filesystem)
 dis_threshold=1
 MSG=""
-
+IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read line
 do
     usage=$( echo $line | awk '{print $6F}' | cut -d "%" -f1 )
@@ -12,4 +12,6 @@ then
     MSG+="High disk usage on $partion: $usage \n"
 fi
 done <<< $disk_usage
-echo -e $MSG
+#echo -e $MSG
+
+sh mail.sh "DEVOPS_TEAM" "High disk usage" "$IP" "$MSG" "shivanathbabu.kodimyala@outlook.com"
