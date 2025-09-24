9e8a6504c4df85c2e69f9ca0bc89e7b676c555dd
ShivanathBabu <shivanathbabu.kodimyala@gmail.com>
2025-09-24 14:22:51 +0530

    shellscripts

--- monitor/disk.sh
+++ monitor/disk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 disk_usage=$(df -hT | grep -v Filesystem)
-dis_threshold= 1
+dis_threshold=1
 MSG= " "
 while IFS = read line
 do
@@ -9,7 +9,7 @@ partion=$(df -hT | awk '{print $7F}')
 
 if [ $usage -gt $dis_threshold ]
 then
-    MSG+= "High disk usage on $partion: $usage %"
+    MSG+= "High disk usage on $partion: $usage \n"
 fi
 done <<< $disk_usage
 echo -e $MSG
