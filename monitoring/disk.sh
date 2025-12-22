c9116debb3c54ac25284429e5cb172695b81ebeb
shivanathbabu kodimyala <shivanathbabu.k@websynergies.com>
2025-12-22 18:22:30 +0530

    Initial commit

---
+++ monitoring/disk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+disk_usage=$(df -hT | grep -v Filesystem)
+disk_threshold=1
+
+while IFS= read line
+do 
+    usage=$(echo $line | awk '{print $6F)' | cut -d "%" -f1)
+    partition=$(echo $line | awk '{print $7F}')
+    if [ $usage -ge $disk_threshold ]
+    then
+        echo "disk usage is high on $partition"
+    fi
+    done <<< $disk_usage
+    
